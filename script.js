ce658951d4dffa936f63d6dd24b3141a5a00ff9a
Raspisaniye-17-Gimnaziya <91901163+Raspisaniye-17-Gimnaziya@users.noreply.github.com>
2021-11-09 11:20:48 +0500

    Update script.js

--- script.js
+++ script.js
@@ -927,7 +927,7 @@ const su = {
 //     sixV: "ЖП"      // 6в
 // }; // База данных на вторник
 
-zagolovokDenNedely.textContent = `Понедельник`;
+zagolovokDenNedely.textContent = `Вторник`;
 
 sixA1.textContent = pn.sixA;
 sixB1.textContent = pn.sixB;
